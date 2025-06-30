8485d6ec0a5d68b23f852444b3029f5250185551
sagarmasal12 <sagarmasal12@gmail.com>
2025-06-30 11:49:43 +0530

    all commit

--- src/app/pages/demo-code/demo-code.component.ts
+++ src/app/pages/demo-code/demo-code.component.ts
@@ -24,6 +24,8 @@ export class DemoCodeComponent {
 
   name = '';
   displayname = "";
+
+  email='';
   getName(event:Event){
     const Name = (event.target as HTMLInputElement).value
     // console.log(Name);
@@ -37,5 +39,14 @@ export class DemoCodeComponent {
     this.name="Sagar"
   }
 
+
+  getEmail(val:string){
+    this.email= val;
+    console.log(val)
+  }
+
+  setEmail(){
+    this.email= 'default@gmail.component';
+  }
 
 }
